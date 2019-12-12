b3deeae14b46bdd1d14631e915f6d53881df9cc6
archuPipon <fernandoalesis@gmail.com>
2019-12-11 21:09:40 -0300

    terminamos secciones procesos y skills

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
-// Rotación de logo
+// Escuchar evento scroll
 $(window).on('scroll', () => {
+    //rotacion de logo
     const scrollRealizado = $(window).scrollTop();
     const giraRuedita = `${scrollRealizado * 0.1}deg`;
     $('#logo').css('transform', `rotate(${giraRuedita})`);
+
+    //aparicion de imagenes
+    const altura = $('.reveal').offset().top; //offset me dice en que posicion absoluta esta dentro de la pagina
+    const diferencia = window.innerHeight * 0.5;
+    if (scrollRealizado >= altura - diferencia) {
+        $('.reveal').addClass('visible');
+    }
 });
 
 // Trazado de SVG
-function trazar(){
-    new Vivus('heroSvg', {
-        duration: 250,
-        type: 'oneByOne',
-        pathTimingFunction: Vivus.EASE_OUT,
-    }, () => {
-        $('#heroSvg').addClass('terminado');
-    });
+function trazar() {
+    new Vivus(
+        'heroSvg',
+        {
+            duration: 250,
+            type: 'oneByOne',
+            pathTimingFunction: Vivus.EASE_OUT,
+        },
+        () => {
+            $('#heroSvg').addClass('terminado');
+        },
+    );
 }
 
 // Tipeo de palabras
-function tipear(){
+function tipear() {
     new TypeIt('#escritura', {
-        strings: ['ELEGANTES', 'MODERNAS', 'RESPONSIVE'],
+        strings: [ 'ELEGANTES', 'MODERNAS', 'RESPONSIVE' ],
         speed: 100,
         deleteSpeed: 50,
         loop: true,
@@ -28,7 +40,7 @@ function tipear(){
 }
 
 // Carrusel 2 (sección intro)
-function carrusel2(){
+function carrusel2() {
     $('#carruselClientes2').owlCarousel({
         items: 3,
         margin: 30,
@@ -66,10 +78,7 @@ $('#stopVid').on('click', () => {
 });
 
 // Smooth Scroll menu navegación
-$('.nav-link').on('click', function(){
+$('.nav-link').on('click', function() {
     const destino = $(this).attr('href');
-    $('html').animate(
-        { scrollTop: $(destino).offset().top },
-        1000,
-    );
+    $('html').animate({ scrollTop: $(destino).offset().top }, 1000);
 });
